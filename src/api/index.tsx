ff6f8725c5d08b007841e48b22a8a305d88c987c
Navdeep Pathan <151203532+navdeeppathan@users.noreply.github.com>
2026-04-01 23:13:55 +0530

    sidebar ui fix

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,35 +1,36 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: `${import.meta.env.VITE_BASE_URL}`
+  baseURL: `${import.meta.env.VITE_BASE_URL}`,
 });
 
 api.interceptors.request.use(
-    async (config) => {
-        const token = sessionStorage.getItem('token');
-        if (token) {
-            config.headers['Authorization'] = `Bearer ${token}`;
-        }
-        return config;
-    },
-    (error) => {
-        console.error('Request error:', error.message);
-        return Promise.reject(error);
+  async (config) => {
+    // const token = sessionStorage.getItem('token');
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
     }
+    return config;
+  },
+  (error) => {
+    console.error("Request error:", error.message);
+    return Promise.reject(error);
+  },
 );
 
 api.interceptors.response.use(
-    (response) => response.data,
-    (error) => {
-        if (error.response?.status === 401) {
-            window.location.href = "/login"; // Redirect to login page
-        }
-        if (error.response?.status === 403) {
-            window.location.href = "/forbidden"; // Redirect to dashboard
-        }
-
-        return Promise.reject(error.response?.data || error.message);
+  (response) => response.data,
+  (error) => {
+    if (error.response?.status === 401) {
+      window.location.href = "/login"; // Redirect to login page
     }
+    if (error.response?.status === 403) {
+      window.location.href = "/forbidden"; // Redirect to dashboard
+    }
+
+    return Promise.reject(error.response?.data || error.message);
+  },
 );
 
 export default api;
